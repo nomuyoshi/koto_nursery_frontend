14a48a7d751decf0c2757688978197251c6d09dd
nomuyoshi <nomuyoshi0111@gmail.com>
2021-08-27 07:43:59 +0900

    検索フォーム

--- components/searchForm.js
+++ components/searchForm.js
@@ -2,36 +2,101 @@ import { useState } from 'react';
 import { KIND_LABELS, MIN_AGE_TYPE_LABELS } from '../constants/labels';
 
 export default function SearchForm() {
-  const [selectedMinAgeType, setMinAgeType] = useState();
-  const [checkedKinds, setCheckedKinds] = useState({});
-  const handleMinAgeTypeChange = (e) => setMinAgeType(e.target.value);
-  const handleKindsChange = (e) => {
-    setCheckedKinds({
-      ...checkedKinds,
-      [e.target.name]: e.target.checked,
+  const [params, setParams] = useState({
+    minAgeType: '',
+    kinds: {},
+    address: '',
+    km: '0.3',
+  });
+
+  const handleChangeParams = (e) => {
+    let newParams = {};
+    const value = e.target.value;
+    const key = e.target.name;
+    switch (e.target.name) {
+      case 'minAgeType':
+      case 'address':
+      case 'km':
+        newParams = {
+          ...params,
+          [key]: value,
+        };
+        break;
+      case 'kinds':
+        const newKinds = {
+          ...params.kinds,
+          [e.target.id]: e.target.checked,
+        };
+        newParams = {
+          ...params,
+          kinds: newKinds,
+        }
+      default:
+        break;
+    }
+
+    setParams(newParams)
+  }
+
+  const onClickSubmit = (e) => {
+    const kinds = [];
+    Object.entries(params.kinds).forEach(([key, value]) => {
+      if (value) { kinds.push(key); } 
     });
-  };
+
+    const reqParams = {
+      minAgeType: params.minAgeType,
+      address: params.address,
+      km: params.km,
+      kinds: kinds,
+    }
+
+    search(reqParams);
+    // TODO: setNurseries
+  }
+
   return (
       <div>
         <label className="label">保育園種別</label>
         {Object.entries(KIND_LABELS).map(([value, label]) => (
           <label className="checkbox" key={value}>
-            <input type="checkbox" name={value} onChange={handleKindsChange} checked={checkedKinds[value] ? true : false}/>
+            <input type="checkbox" id={value} name="kinds" onChange={handleChangeParams} checked={params.kinds[value] ? true : false} />
             {label}
           </label>
         ))}
         <label className="label">保育開始年齢</label>
         <div className="select">
-          <select value={selectedMinAgeType} onChange={handleMinAgeTypeChange}>
+          <select name="minAgeType" onChange={handleChangeParams} value={params.minAgeType}>
             <option key="none">問わない</option>
             {Object.entries(MIN_AGE_TYPE_LABELS).map(([value, label]) => (
               <option key={value} value={value}>{label}〜</option>
             ))}
           </select>
         </div>
+        <div className="field has-addons has-addons-centered">
+          <p className="control">
+            <input name="address" className="input" type="text" placeholder="住所" onChange={handleChangeParams} value={params.address} />
+          </p>
+          <p className="control">
+            <span className="select">
+              <select name="km" onChange={handleChangeParams} value={params.km}>
+                <option value="0.3">300m以内</option>
+                <option value="0.5">500m以内</option>
+                <option value="1.0">1km以内</option>
+                <option value="1.5">1.5km以内</option>
+              </select>
+            </span>
+          </p>
+        </div>
         <div className="control">
-          <button className="button is-primary">検索</button>
+          <button className="button is-primary" onClick={onClickSubmit}>検索</button>
         </div>
       </div>
   );
 };
+
+async function search(params = {}) {
+  console.log(params);
+  // TODO
+}
+
