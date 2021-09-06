75342ea8831f892d50e1d89194ac77909b40ba0d
nomuyoshi <nomuyoshi0111@gmail.com>
2021-09-06 17:52:26 +0900

    GA設定

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ module.exports = {
     MAPS_EMBED_API_KEY: process.env.MAPS_EMBED_API_KEY,
     SITE_BASE_URL: process.env.SITE_BASE_URL,
     SITE_TOP_URL: process.env.SITE_TOP_URL,
+    GA_ID: process.env.GA_ID,
   },
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import '../styles/bulma.min.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
